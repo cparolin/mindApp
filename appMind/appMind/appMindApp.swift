2c3739d83a4a9f8f978546c0d95e677151a95fb6
Camila Parolin <camilaparolin10@gmail.com>
2025-10-14 16:10:57 -0300

    Xcode project

---
+++ appMind/appMind/appMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  appMindApp.swift
+//  appMind
+//
+//  Created by Camila Parolin on 14/10/25.
+//
+
+import SwiftUI
+
+@main
+struct appMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
