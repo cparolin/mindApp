5c40073e22b981c5587c390e0e70db6a68452cd3
Dayo Araujo <dayo.araujosz@gmail.com>
2025-10-14 16:44:56 -0300

    feat: journal model

--- appMind/appMind/Model/JornalModel.swift
+++ appMind/appMind/Model/JornalModel.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+enum JournalType: String, CaseIterable {
+    case socializacao = "Socialização"
+    case vicio = "Vício"
+    case rotina = "Rotina"
+}
+
+struct JournalModel {
+    var id: UUID = UUID()
+    var type: JournalType
+    var title: String
+    var description: String
+    var date: Date
+    var answers: [String]
+}
