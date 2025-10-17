f8d9f629659aada69b72a4bdd59d50c180e1d7d1
LuccaPV <luccappadilha@gmail.com>
2025-10-17 15:12:35 -0300

    Feat: Tela de edicao das tesk parcialmente funcionando

--- appMind/appMind/View/EditingTaksView.swift
+++ appMind/appMind/View/EditingTaksView.swift
@@ -1,37 +1,31 @@
 //
-//  NewTaskView.swift
-//  TodoPOC
+//  EditingTaksView.swift
+//  appMind
 //
-//  Created by André Contarelli Lima on 08/10/25.
+//  Created by Lucca Pivoto on 17/10/25.
 //
+
 import SwiftData
 import SwiftUI
-
-struct NewTaskView: View {
+///Precisa garantir a trocva de cor, não está funcionando
+struct EditingTaskView: View {
     @Environment(\.dismiss) private var dismiss
     @Environment(\.modelContext) var modelContext
-    @State var enunToString: EnunsCreateEditTaskVIew = EnunsCreateEditTaskVIew()
     @Query var tasks: [Task]
-    @State var taskTitle: String = ""
-    @State var taskNote: String = ""
-    @State var taskDate: Date = Date()
-    @State var taskColor: String = ""
+    @State var enunToString: EnunsCreateEditTaskVIew = EnunsCreateEditTaskVIew()
+    @State var taskBeengEditing: Task
     @State var selectedColor: Color = .yellow
-    let isEditing: Bool = false
-    @Binding var newTask: Task
-    
-    
     var body: some View {
         NavigationStack {
             VStack(alignment: .leading, spacing: 15, content: {
                 VStack(alignment: .leading, spacing: 8, content: {
                     
-                    TextField("Nome do evento", text: $taskTitle)
+                    TextField("Nome do evento", text: $taskBeengEditing.taskTitle)
                         .padding(.vertical, 12)
                         .padding(.horizontal, 16)
                         .font(.title)
                     
-                    TextField("Notas", text: $taskNote)
+                    TextField("Notas", text: $taskBeengEditing.notes)
                         .padding(.horizontal, 16)
                 })
                 Divider()
@@ -43,7 +37,7 @@ struct NewTaskView: View {
                             .font(.caption)
                             .foregroundStyle(.gray)
                         
-                        DatePicker("", selection: $taskDate)
+                        DatePicker("", selection: $taskBeengEditing.todoDate)
                             .datePickerStyle(.compact)
                             .scaleEffect(0.9, anchor: .leading)
                     })
@@ -74,7 +68,7 @@ struct NewTaskView: View {
                                     .onTapGesture {
                                         withAnimation(.snappy) {
                                             selectedColor = color
-                                            taskColor = corPasta(selectedColor)
+//                                            $taskBeengEditing.taskColor = enunToString.corPasta(selectedColor)
                                         }
                                     }
                             }
@@ -97,18 +91,76 @@ struct NewTaskView: View {
                 }
                 ToolbarItem(placement: .confirmationAction){
                     Button(action: {
-                        newTask = Task(taskTitle: "\(taskTitle)", todoDate: taskDate, isCompleted: false, tint: "\(taskColor)", notes: "\(taskNote)")
-                        modelContext.insert(newTask)
+                        do{
+                           try modelContext.save()
+                        }
+                        catch let error{
+                            print(error.localizedDescription)
+                        }
                         dismiss()
                     }, label: {
-                        Text("OK")
+                        Text("Salvar")
                     })
-                    .disabled(taskTitle == "" || taskNote == "")
+//                    .disabled(taskTitle == "" || taskNote == "")
                 }
             }
         }
     }
 }
+
+enum CorTarefa: String {
+    
+    case blue
+    case red
+    case yellow
+    case orange
+    case purple
+    case gray
+    
+    var color: Color {
+        switch self {
+        case .blue:
+            return .blue
+            
+        case .red:
+            return .red
+            
+        case .yellow:
+            return .yellow
+            
+        case .orange:
+            return .orange
+            
+        case .purple:
+            return .purple
+            
+        case .gray:
+            return .gray
+        }
+    }
+}
+
+func corPasta(_ cor: Color) -> String {
+    var corString: String = "erro"
+    switch cor{
+    case .yellow:
+        corString = "yellow"
+    case .blue:
+        corString = "blue"
+    case .red:
+        corString = "red"
+    case .orange:
+        corString = "orange"
+    case .purple:
+        corString = "purple"
+        
+    default:
+        corString = "gray"
+    }
+    return corString
+       }
+
 #Preview {
     ContentView()
 }
+
