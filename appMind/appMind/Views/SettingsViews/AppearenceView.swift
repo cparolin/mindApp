df848cf33151922b752b037f1e76b9b38c755d31
Camila Parolin <camilaparolin10@gmail.com>
2025-10-17 15:25:16 -0300

    feat settings update

--- appMind/appMind/Views/SettingsViews/AppearenceView.swift
+++ appMind/appMind/Views/SettingsViews/AppearenceView.swift
@@ -6,108 +6,21 @@
 //
 
 import SwiftUI
+import AudioToolbox
 
 struct AppearenceView: View {
     
-    @State private var colorLayout: ColorCases = .amarelo
+    @AppStorage("colorLayout") private var colorLayout: ColorCases = .amarelo
+    @AppStorage("textLayout") private var textLayout: Int = 0
+    @AppStorage("sound") private var sound = true
+    @AppStorage("tactile") private var tactile = true
+    @AppStorage("font") private var font: Int = 0
     
     var body: some View {
         NavigationStack {
-            VStack {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 20)
-                        .frame(width: UIScreen.main.bounds.width * 0.93, height: 230)
-                        .foregroundStyle(.tertiary)
-                    VStack{
-                        HStack{
-                            Spacer()
-                                .frame(width: 5)
-                            
-                            ForEach((1...6), id: \.self) { number in
-                                if number == 5 {
-                                    RoundedRectangle(cornerRadius: 12)
-                                        .frame(width: UIScreen.main.bounds.width * 0.10, height: 55)
-                                        .foregroundStyle(Color("\(colorLayout)"))
-                                        .padding(.trailing, 5)
-                                }
-                                RoundedRectangle(cornerRadius: 12)
-                                    .frame(width: UIScreen.main.bounds.width * 0.10, height: 55)
-                                    .foregroundStyle(.secondary)
-                                    .padding(.trailing, 5)
-                            }
-                        }
-                        
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 16)
-                                .frame(width: UIScreen.main.bounds.width * 0.89, height: 32)
-                                .foregroundStyle(.secondary)
-                            
-                            HStack {
-                                Text("15:00")
-                                
-                                Rectangle()
-                                    .frame(width: 100, height: 5)
-                                
-                                Spacer()
-                                    .frame(width: 150)
-                                
-                                Circle()
-                                    .stroke(.black)
-                                    .fill(.secondary)
-                                    .frame(width: 20)
-                            }
-                            
-                        }
-                        
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 16)
-                                .frame(width: UIScreen.main.bounds.width * 0.89, height: 32)
-                                .foregroundStyle(.secondary)
-                            
-                            HStack {
-                                Text("17:30")
-                                
-                                Rectangle()
-                                    .frame(width: 100, height: 5)
-                                
-                                Spacer()
-                                    .frame(width: 150)
-                                
-                                Circle()
-                                    .stroke(.black)
-                                    .fill(.secondary)
-                                    .frame(width: 20)
-                            }
-                        }
-                        
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 16)
-                                .frame(width: UIScreen.main.bounds.width * 0.89, height: 50)
-                                .foregroundStyle(.secondary)
-                            
-                            HStack {
-                                Text("17:30")
-                                
-                                VStack (alignment: .leading){
-                                    Rectangle()
-                                        .frame(width: 100, height: 5)
-                                    
-                                    Rectangle()
-                                        .frame(width: 200, height: 5)
-//                                        .padding(.leading, 100)
-                                }
-                                
-                                Spacer()
-                                    .frame(width: 50)
-                                
-                                Circle()
-                                    .stroke(.black)
-                                    .fill(.secondary)
-                                    .frame(width: 20)
-                            }
-                        }
-                    }
-                }
+            ScrollView {
+                
+                LayoutRectangle()
                 
                 HStack {
                     Text("Cor Principal")
@@ -155,12 +68,60 @@ struct AppearenceView: View {
                     }
                 }
                 
+                ///Text Layout
                 HStack {
                     Text("Tipo de Layout")
                         .font(.title)
                         .bold()
                     
                     Spacer()
+                }
+                .padding()
+                
+                Picker("", selection: $textLayout) {
+                    Text("Completo").tag(0)
+                    Text("Simplificado").tag(1)
+                    Text("Mínimo").tag(2)
+                }
+                .pickerStyle(.segmented)
+                .frame(width: UIScreen.main.bounds.width * 0.93)
+                
+                HStack {
+                    Text("Efeitos")
+                        .font(.title)
+                        .bold()
+                    
+                    Spacer()
+                }
+                .padding()
+                
+//                List {
+                
+                    Toggle("Som", isOn: $sound)
+                    .onChange(of: sound) {
+                        AudioServicesPlaySystemSound(1522)
+                    }
+                    .padding()
+                
+                Toggle("Tátil", isOn: $tactile)
+                .onChange(of: tactile) {
+                    AudioServicesPlaySystemSound(1522)
+                }
+                .padding()
+//                }
+                
+                HStack {
+                    Text("Fonte")
+                        .font(.title)
+                        .bold()
+                    
+                    Spacer()
+                    
+                    
+                    Picker("", selection: $font) {
+                        Text("SF Pro").tag(0)
+                        Text("OpenDislexic").tag(1)
+                    }
                 }
                 .padding()
                 
