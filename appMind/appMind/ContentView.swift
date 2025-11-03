35565485e2f871480a250c124c8daebaca19a362
Camila Parolin <camilaparolin10@gmail.com>
2025-11-03 14:22:40 -0300

    changed content view

--- appMind/appMind/ContentView.swift
+++ appMind/appMind/ContentView.swift
@@ -31,7 +31,7 @@ struct ContentView: View {
                     .tabItem {
                         Label("Diários", systemImage: "book.pages")
                     }
-                SettingsView()
+                AppearenceView()
                     .tabItem {
                         Label("Configurações", systemImage: "gear")
                     }
